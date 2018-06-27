fe288a098d97edd5efe9397e4df62dc58e97eaf2
fhddwxd <727630921@qq.com>
2018-06-27 15:35:11 +0800

    update

--- app/src/main/java/com/wxd/myutils/MainActivity.kt
+++ app/src/main/java/com/wxd/myutils/MainActivity.kt
@@ -2,11 +2,14 @@ package com.wxd.myutils
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.wxd.myutils.Views.ClockView
 
 class MainActivity : AppCompatActivity() {
-
+    private var clockView:ClockView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clockView = findViewById(R.id.myClock)
+        clockView!!.setCalendar()
     }
 }
